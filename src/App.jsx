d5473e872a16a5d7e913ef6d31c03c6957c01c35
Anda <ax.fiolle@gmail.com>
2018-01-08 16:54:05 +0100

    it works

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import './css/app.css';
 
 import AllOver from './views/allover.jsx';
@@ -7,57 +8,24 @@ import ToDoForm from './views/todoform.jsx';
 
 const jsonFile = 'https://andaroth.github.io/learning-reactjs/src/json/list.json';
 
-
-
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.jsondata;
-    /*this.jsondata = [
-      {
-        "done":true, 
-        "txt":"Dormir chez un Chinois pour son anniversaire"
-      },
-      {
-        "done":false, 
-        "txt":"Utiliser Wix"
-      },
-      {
-        "done":true,
-        "txt":"Installer WordPress"
-      }
-    ] // json end*/
+    this.state;
   } // constructor end -------------------------
+  state = {myList:[]};
   componentWillMount() {
-    
-    var loadFile = (jsonFile) => {
-    var xhr = new XMLHttpRequest()
-    xhr.open('get', jsonFile, true)
-    xhr.responseType = 'json'
-    xhr.onreadystatechange = function() {
-      var status;
-      if (xhr.readyState == 4) { // done
-        console.log("4");
-        status = xhr.status;
-        if (status == 200) { // okay
-          console.log("okay");
-          console.log("result : "+xhr.response)
-          this.jsondata = xhr.response;
-          ToDoList.mapThis(this.jsondata);
-        } else {
-          console.log("cant find !")
-        }
-      } // readyState=4 end
-    } // onreadystatechange end
-    xhr.send()
-  } // loadfile end
-  }
+    axios.get(jsonFile) // load this
+      .then((res) => {
+        this.setState({myList:res.data}) // and setstate to json
+      })
+  } // willmount end
   render() {
     return (
       <div className="main">
         <AllOver />
         <ToDoForm />
-        <ToDoList jsondata={this.jsondata} />
+        <ToDoList jsondata={this.state.myList} />
       </div>
     ); // return() end
   } // render end
--- src/views/todolist.jsx
+++ src/views/todolist.jsx
@@ -5,27 +5,31 @@ constructor(props) {
   super(props)
 } // constructor end -------------------------
 mapThis = (thisList) => {
+  console.log("props : " + JSON.stringify(this.props.jsondata));
   if (thisList) {
     // Mapper dans "this.state" pour écrire toutes les entrées
-    this.jsondata.map((i) => {
-      var done = i.done,txt = i.txt, id = i.id;
-      done = !(done) ? 'ToDo' : 'Done';
-      return(
-        <li key={id}>
-          <span className={done}>{done}</span>
-          <span className="txt">{txt}</span>
-        </li>
-       ) // map-return end
-    }) // map end
+    return(
+      thisList.map((i,index) => {
+//        var done = i.done, txt = i.txt, id = i.id;
+        i.done = !(i.done) ? 'ToDo' : 'Done';
+        return(
+          <li key={index}>
+            <span className={i.done}>{i.done}</span>
+            <span className="txt">{i.txt}</span>
+          </li>
+         ) // map-return end
+      }) // map end
+    ) // mapThis return end
   } else {
-    return(<li><p className="center">Please wait while loading... </p></li>)
+    return(<li key="no"><p className="center">Please wait while loading... </p></li>)
   }
 }
 render() {
   return(
     <div className="todolist">
       <ul id="myList">
-        {this.mapThis(this.props.jsondata)} 
+        {
+          this.mapThis(this.props.jsondata)} 
       </ul>
     </div>
   ) // return end
